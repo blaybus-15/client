0b7c379fe55f086585e5118dd35b77f22aeacdc9
최하은 <haeun4345@gmail.com>
2025-02-14 01:45:46 +0900

    feat: 네비게이션바 추가 (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
+import NavBar from './components/NavBar';
+
+const AppContent = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <div className="min-h-screen bg-gray-200">
+      {!isHomePage && (
+        <div className="fixed top-0 left-0 z-50 w-full">
+          <div className="max-w-3xl mx-auto ">
+            <NavBar />
+          </div>
+        </div>
+      )}
+      <div className={'max-w-3xl mx-auto'}>
+        <Routes>
+          <Route path="/" />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-200">
-      <div className="max-w-3xl min-h-screen mx-auto">
-        <Router>
-          <Routes>
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </Router>
-      </div>
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   );
 };
 
