13c497a363252408b178bc3e2a5b34c1d8b380e0
ssuzn <tnwls_723@naver.com>
2025-02-14 22:45:19 +0900

    feat: 가입 완료 페이지 구현 (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import SignUpPage from './pages/SignUpPage';
 import Step1 from "./pages/auth/signup/common/Step1";
 import Step2 from "./pages/auth/signup/common/Step2";
 import Step3 from "./pages/auth/signup/common/Step3";
+import Success from './pages/auth/signup/common/Success';
 import LoginPage from './pages/LoginPage';
 import NavBar from './components/NavBar';
 
@@ -32,6 +33,7 @@ const AppContent = () => {
           <Route path="/signup/step1" element={<Step1 />} />
           <Route path='/signup/step2' element={<Step2 />} />
           <Route path="/signup/step3" element={<Step3 />} />
+          <Route path="/signup/success" element={<Success />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
---
+++ src/pages/auth/signup/common/Success.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import Button from '../../../../components/Button';
+
+const Success = () => {
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const userType = searchParams.get("type") || "caregiver";
+
+    const handleNext = () => {
+        if (userType === "caregiver") {
+            navigate("/profile-setup");
+        } else {
+            navigate("/elderly-registration");
+        }
+    };
+
+    return (
+        <div className="flex flex-col justify-evenly min-h-screen px-6 py-12 bg-white">
+            <div className="flex flex-col flex-1 justify-center items-center text-center space-y-6">
+            <h2 className="text-2xl font-bold">가입 완료</h2>
+
+            {userType === "caregiver" && (
+            <p className="text-lg text-gray-700">
+                프로필 등록하고 집 근처 일자리 정보를 찾아보세요!
+            </p>
+            )}
+        </div>
+
+        <div className="w-full px-8 pb-12">   
+            <Button
+                text={userType === "caregiver" ? "프로필 등록하러 가기" : "어르신 정보 등록"}
+                onClick={handleNext}
+            />
+        </div>
+    </div>
+    )
+}
+
+export default Success;
