2a22f39b18867d8514f064c48c49e9b38a912ef7
최하은 <haeun4345@gmail.com>
2025-02-16 03:14:52 +0900

    chore: 기본 폰트 및 컬러칩 추가 (#7)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard'],
+      },
+      colors: {
+        main: '#FFE943',
+        dark: '#221313',
+        background: {
+          point: '#FFFEDF',
+          gray: '#F3F4F5',
+        },
+        gray: {
+          1: '#606265',
+          2: '#B1B5B9',
+        },
+      },
+    },
   },
   plugins: [],
 };
