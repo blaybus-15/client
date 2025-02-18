3f96af986811d3ed4c21b0ffde830325b06f69e0
최하은 <haeun4345@gmail.com>
2025-02-18 18:04:59 +0900

    fix: 텍스트 필드 입력 길이에 따라 높이 조절 (#12)

--- src/components/LimitedTextField.jsx
+++ src/components/LimitedTextField.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const LimitedTextInput = ({
   value,
@@ -7,6 +7,24 @@ const LimitedTextInput = ({
   placeholder = '입력해주세요.',
   className = '',
 }) => {
+  const textAreaRef = useRef(null);
+  const [rows, setRows] = useState(1);
+
+  useEffect(() => {
+    const textAreaLineHeight = 24;
+    const previousRows = textAreaRef.current.rows;
+    textAreaRef.current.rows = 1;
+    const currentRows = Math.floor(
+      textAreaRef.current.scrollHeight / textAreaLineHeight
+    );
+
+    if (currentRows === previousRows) {
+      textAreaRef.current.rows = currentRows;
+    }
+
+    setRows(currentRows);
+  }, [value]);
+
   const handleChange = (e) => {
     const input = e.target.value;
     if (input.length <= maxLength) {
@@ -15,13 +33,15 @@ const LimitedTextInput = ({
   };
 
   return (
-    <div className="w-full ">
+    <div className="w-full">
       <div className="relative rounded-lg body-regular-16">
         <textarea
+          ref={textAreaRef}
           value={value}
           onChange={handleChange}
           placeholder={placeholder}
-          className={`w-full p-4 min-h-[100px] rounded-lg bg-background-gray resize-none outline-none placeholder-gray-1 text-dark ${className}`}
+          rows={rows}
+          className={`w-full p-4 rounded-lg bg-background-gray resize-none outline-none placeholder-gray-1 text-dark ${className}`}
         />
         <div className="absolute caption-regular-14 bottom-3 right-3">
           <span className="text-red-400">{value.length}</span>
