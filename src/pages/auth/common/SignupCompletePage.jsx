f4127099118bd91bebc621292eba6fa34960f720
ssuzn <tnwls_723@naver.com>
2025-02-19 20:34:04 +0900

    feat: 일러스트 적용 및 서비스 이름 수정 (#27)

--- src/pages/auth/common/SignupCompletePage.jsx
+++ src/pages/auth/common/SignupCompletePage.jsx
@@ -2,52 +2,69 @@ import React from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import Button from '../../../components/Button';
 import { useDispatch, useSelector } from 'react-redux';
+import Illustration from '../../../assets/complete.png';
 
 const SignupCompletePage = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [searchParams] = useSearchParams();
-    const userType = searchParams.get("type") || "CAREGIVER";
-    const role = useSelector((state) => state.auth.role) || localStorage.getItem("role") || userType;;
+  const [searchParams] = useSearchParams();
+  const userType = searchParams.get('type') || 'CAREGIVER';
+  const role =
+    useSelector((state) => state.auth.role) ||
+    localStorage.getItem('role') ||
+    userType;
 
-    const handleNext = () => {
-        if (role === "CAREGIVER") {
-            navigate("/profile/caregiver");
-        } else {
-            navigate("/profile/senior");
-        }
-    };
+  const handleNext = () => {
+    if (role === 'CAREGIVER') {
+      navigate('/profile/caregiver');
+    } else {
+      navigate('/profile/senior');
+    }
+  };
 
-    return (
-        <div className="flex flex-col justify-evenly min-h-screen px-6 pt-12 bg-white">
-            <div className="flex flex-col flex-1 items-center text-center mt-7 space-y-6">
-                <h2 className="head-semi-bold-24 leading-[40px] text-dark">
-                    {role === "CAREGIVER" ? (
-                        <>
-                            프로필 등록하고<br />집근처 일자리 정보를<br />찾아보세요!
-                        </>
-                    ) : (
-                        <>
-                            어르신의 정보를 입력하고<br />간편하게 업무 관리를<br />해보세요!
-                        </>
-                    )
-                    }
-                </h2>
+  return (
+    <div className="flex flex-col justify-evenly min-h-screen px-6 pt-12 bg-white">
+      <div className="flex flex-col flex-1 items-center text-center mt-7 ">
+        <h2 className="head-semi-bold-24 leading-[40px] text-dark">
+          {role === 'CAREGIVER' ? (
+            <>
+              프로필 등록하고
+              <br />
+              집근처 일자리 정보를
+              <br />
+              찾아보세요!
+            </>
+          ) : (
+            <>
+              어르신의 정보를 입력하고
+              <br />
+              간편하게 업무 관리를
+              <br />
+              해보세요!
+            </>
+          )}
+        </h2>
 
-                <div className="flex mt-12 justify-center items-center w-[300px] h-[400px] bg-gray-200">
-                    <span className="text-gray-500">일러스트</span>
-                </div>
-            </div>
+        <img
+          src={Illustration}
+          alt="일러스트"
+          className="min-h-full max-h-[60vh] flex justify-center items-center"
+        />
+      </div>
 
-        <div className="mb-5 justify-end">   
-            <Button
-                text={role === "CAREGIVER" ? "프로필 등록하러 가기" : "어르신 정보 입력하러 가기"}
-                onClick={handleNext}
-            />
-        </div>
+      <div className="mb-5 justify-end">
+        <Button
+          text={
+            role === 'CAREGIVER'
+              ? '프로필 등록하러 가기'
+              : '어르신 정보 입력하러 가기'
+          }
+          onClick={handleNext}
+        />
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default SignupCompletePage;
--- src/pages/auth/common/SocialAuthPage.jsx
+++ src/pages/auth/common/SocialAuthPage.jsx
@@ -1,59 +1,81 @@
-import React from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import Button from "../../../components/Button";
-import KakaoIcon from "../../../assets/kakaotalk-icon.svg";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import Button from '../../../components/Button';
+import KakaoIcon from '../../../assets/kakaotalk-icon.svg';
+import { useSelector } from 'react-redux';
+import Illustration from '../../../assets/auth.png';
 
 const SocialAuthPage = () => {
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const userType = searchParams.get("type") || "CAREGIVER";
-    const role = useSelector((state) => state.auth.role);
-    const handleKakaoStart = () => {
-        console.log("kakao start");
-    };
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const userType = searchParams.get('type') || 'CAREGIVER';
+  const role = useSelector((state) => state.auth.role);
+  const handleKakaoStart = () => {
+    console.log('kakao start');
+  };
 
-    const handleLogin = () => {
-        navigate(`/login?type=${role}`);
-    };
+  const handleLogin = () => {
+    navigate(`/login?type=${role}`);
+  };
 
-    const handleSignup = () => {
-        if (role === "CAREGIVER") {
-            navigate("/signup/caregiver/profile");
-        } else if (role === "ADMIN") {
-            navigate("/signup/admin/center/register");
-        } else {
-            navigate("/signup"); // 예외 처리
-        }
-    };
+  const handleSignup = () => {
+    if (role === 'CAREGIVER') {
+      navigate('/signup/caregiver/profile');
+    } else if (role === 'ADMIN') {
+      navigate('/signup/admin/center/register');
+    } else {
+      navigate('/signup'); // 예외 처리
+    }
+  };
 
-    return (
-        <div className="flex flex-col justify-between min-h-screen px-6 py-12 bg-white">
-            <div className="flex-1"></div>
+  return (
+    <div className="flex flex-col justify-between min-h-screen px-6 pt-12 bg-white">
+      <div className="flex flex-col flex-1 justify-evenly">
+        <h2 className="head-semi-bold-24 text-dark text-left leading-[40px] mb-6">
+          {role === 'ADMIN' ? (
+            <>
+              센터의 첫걸음,
+              <br />
+              다온과 함께 시작하세요!
+            </>
+          ) : (
+            <>
+              요양보호사의 첫걸음,
+              <br />
+              다온과 함께 시작하세요!
+            </>
+          )}
+        </h2>
+        <img src={Illustration} alt="일러스트" className="w-full h-auto" />
+      </div>
 
-            <div className="space-y-4">
-                <button
-                    className="relative body-semi-bold-18 w-full py-4 bg-main text-black rounded-2xl transition hover:bg-opacity-80
+      <div className="space-y-4">
+        <button
+          className="relative body-semi-bold-18 w-full py-4 bg-main text-black rounded-2xl transition hover:bg-opacity-80
                     flex items-center justify-center"
-                    onClick={handleKakaoStart}
-                >
-                    <img src={KakaoIcon} alt="카카오 아이콘" className="absolute left-[15px] w-6 h-6" />
-                    <span>카카오로 시작하기</span>
-                </button>
-                <Button text="로그인 하기" onClick={handleLogin} />
-            </div>
+          onClick={handleKakaoStart}
+        >
+          <img
+            src={KakaoIcon}
+            alt="카카오 아이콘"
+            className="absolute left-[15px] w-6 h-6"
+          />
+          <span>카카오로 시작하기</span>
+        </button>
+        <Button text="로그인 하기" onClick={handleLogin} />
+      </div>
 
-            <p className="text-center caption-regular-14 text-gray-2 mt-4">
-                케어링크가 처음이신가요?{" "}
-                <span 
-                    className="inline-block ml-5 text-dark font-semibold cursor-pointer"
-                    onClick={handleSignup}    
-                >
-                    회원가입하기
-                </span>
-            </p>
-        </div>
-    );
+      <p className="text-center caption-regular-14 text-gray-2 my-4">
+        다온이 처음이신가요?{' '}
+        <span
+          className="inline-block ml-5 text-dark font-semibold cursor-pointer"
+          onClick={handleSignup}
+        >
+          회원가입하기
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default SocialAuthPage;
