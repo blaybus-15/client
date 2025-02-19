e556ad2dcb4db86f037cae9ca922c9f8b97ab3ac
ssuzn <tnwls_723@naver.com>
2025-02-19 21:10:04 +0900

    style: 프로필 등록 완료 시 아이콘 추가 (#27)

--- src/pages/profile/caregiver/steps/SuccessPage.jsx
+++ src/pages/profile/caregiver/steps/SuccessPage.jsx
@@ -1,6 +1,6 @@
 import Button from '../../../../components/Button';
 import { useNavigate } from 'react-router-dom';
-
+import CheckIcon from '../../../../assets/check-icon.svg';
 const SuccessPage = () => {
   const navigate = useNavigate();
 
@@ -10,16 +10,20 @@ const SuccessPage = () => {
 
   return (
     <div className="flex flex-col min-h-screen px-4 pt-12 bg-white justify-evenly">
-      <div className="flex flex-col items-center justify-center flex-1 space-y-6 text-center">
+      <div className="text-center mt-12">
         <h2 className="head-semi-bold-24 leading-[40px] text-dark">
           프로필 등록을
           <br />
           완료했습니다!
         </h2>
+      </div>
 
-        <div className="flex mt-12 justify-center items-center w-[247px] h-[313px] bg-gray-200">
-          <span className="text-gray-500">일러스트</span>
-        </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img
+          src={CheckIcon}
+          alt="체크 아이콘"
+          className="w-40 h-40 self-center"
+        />
       </div>
 
       <div className="w-full pb-12">
